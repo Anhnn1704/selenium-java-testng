6fa31c743f7da8e927bfe5656aaf9196b4da158f
Automation Test <anhnn1704@gmail.com>
2025-06-15 09:14:44 +0700

    Update Topic 01

--- selenium/webdriver/Topic_02_Selenium_Locator.java
+++ selenium/webdriver/Topic_02_Selenium_Locator.java
@@ -12,120 +12,97 @@ public class Topic_02_Selenium_Locator {
 
     WebDriver driver;
 
-    String fullName = "Selenium Locator";
+   // String fullName = "Selenium Locator";
 
     @BeforeClass
     public void initialBrowser(){
         //Mở browser lên
         driver = new FirefoxDriver();
         // Mở app lên đến màn hình Login
-        driver.get("https://demo.nopcommerce.com/register");
+        driver.get("https://vio.edu.vn/register/step-one");
     }
 
     @Test
-    public void TC_01_ID(){
-        driver.findElement(By.id("small-searchterms"));
-
-        driver.findElement(By.id("FirstName"));
+    public void TC_01_ID() throws InterruptedException {
+        driver.findElement(By.name("fullName")).sendKeys("Nguyen Ngoc Anh");
+        driver.findElement(By.name("username")).sendKeys("anhnn1704");
+        driver.findElement(By.name("password")).sendKeys("Anh@1234");
+        driver.findElement(By.name("passwordConfirm")).sendKeys("Anh@1234");
+        driver.findElement(By.name("phone")).sendKeys("0372371927");
+        Thread.sleep(3000);
     }
 
     @Test
-    public void TC_02_Class(){
+    public void TC_02_Class() throws InterruptedException {
         //Nó ko lấy hết toàn bộ giá trị (nếu có khoảng trắng)
-         driver.findElement(By.className("register-next-step-button"));
+         driver.findElement(By.className("btn-register-form")).click();
+        Thread.sleep(3000);
     }
 
     @Test
-    public void TC_03_Name(){
-        driver.findElement(By.name("DateOfBirthDay"));
-        driver.findElement(By.name("DateOfBirthMonth"));
-        driver.findElement(By.name("DateOfBirthYear"));
+    public void TC_03_Name() throws InterruptedException {
+        driver.findElement(By.name("fullName"));
+        Thread.sleep(3000);
     }
 
     @Test
-    public void TC_04_LinkText(){
+    public void TC_04_LinkText() throws InterruptedException {
         //Chỉ làm việc với element và link và có text
         //Thẻ a và có thuộc tính href
         //Phải lấy hết toàn bộ text ko chứa cái nào hết (tuyệt đối)
-        driver.findElement(By.linkText("Register"));
-
-        driver.findElement(By.linkText("Log in"));
-
-        driver.findElement(By.linkText("WishList"));
+        driver.findElement(By.linkText("support@vio.edu.vn"));
+        Thread.sleep(3000);
+        driver.findElement(By.linkText("Facebook VioEdu")).click();
+        Thread.sleep(3000);
     }
 
     @Test
     public void TC_05_Partial_Link_Text(){
         //Chỉ làm việc với element và link
         //Có thể lấy hết toàn bộ hoặc 1 phần (hay dùng)
-        driver.findElement(By.partialLinkText("Register"));
-
-        driver.findElement(By.partialLinkText("Digital"));
-
-        driver.findElement(By.partialLinkText("dowloads"));
+        driver.findElement(By.partialLinkText("Đổi")).click();
+        driver.findElement(By.partialLinkText("quà")).click();
     }
 
     @Test
     public void TC_06_Tagname(){
         //Tên thẻ (HTML)
         //Tìm tất cả các element giống nhau (thẻ của component giống nhau)
-        //Tất cả textbox/ checkbox/ radioa/ link/ button/ ...
+        //Tất cả textbox/ checkbox/ radio/ link/ button/ ...
         driver.findElement(By.tagName("button"));
-
-        driver.findElement(By.tagName("input"));
-
-        driver.findElement(By.tagName("lable"));
+        driver.findElement(By.tagName("a"));
     }
 
     @Test
     public void TC_07_Css(){
-        driver.findElement(By.cssSelector("input#Company"));
-        driver.findElement(By.cssSelector("#Company"));
-        driver.findElement(By.cssSelector("input[id='Company']"));
+        driver.findElement(By.cssSelector("input[name='fullName']"));
 
-        driver.findElement(By.cssSelector("button.register-next-step-button"));
-        driver.findElement(By.cssSelector("button[class='button-1 register-next-step-button']"));
+        driver.findElement(By.cssSelector("button.btn-register-form"));
+        driver.findElement(By.cssSelector("button[class='btn btn-register-form btn-green-new']"));
 
-        driver.findElement(By.cssSelector("select[name='DateOfBirthDay']"));
-        driver.findElement(By.cssSelector("select[name='DateOfBirthMonth']"));
-        driver.findElement(By.cssSelector("select[name='DateOfBirthYear']"));
+        driver.findElement(By.cssSelector("a[href='/skill-list']"));
+        driver.findElement(By.cssSelector("a[href='/arena']"));
 
-        driver.findElement(By.cssSelector("a[href*='/register?returnUrl=%2F']"));
-        driver.findElement(By.cssSelector("a[href*='/login?returnUrl=%2F?']"));
-
-        driver.findElement(By.cssSelector("a[href*='register?']"));
-        driver.findElement(By.cssSelector("a[href*='login?']"));
-
-        driver.findElement(By.cssSelector("a"));
         driver.findElement(By.cssSelector("button"));
         driver.findElement(By.cssSelector("input"));
     }
 
     @Test
     public void TC_08_XPath(){
-        driver.findElement(By.xpath("//input[@id='small-searchterms']"));
-        driver.findElement(By.xpath("//input[@id='Password'"));
-        driver.findElement(By.xpath("//input[@id='Company'"));
+        driver.findElement(By.xpath("//input[@name='fullName']"));
 
-        driver.findElement(By.xpath("//button[@class='button-1 register-next-step-button']"));
-        driver.findElement(By.xpath("//button[contains(@class, 'register-next-step-button')]"));
+        driver.findElement(By.xpath("//button[@class='btn btn-register-form btn-green-new']"));
+        driver.findElement(By.xpath("//button[contains(@class, 'btn-register-form')]"));
 
-        driver.findElement(By.xpath("//select[@name='DateOfBirthDay']"));
-        driver.findElement(By.xpath("//select[@name='DateOfBirthMonth']"));
-        driver.findElement(By.xpath("//select[@name='DateOfBirthYear']"));
+        driver.findElement(By.xpath("//a[text()='Mua khóa học']"));
 
-        driver.findElement(By.xpath("//a[text()='Register']"));
-        driver.findElement(By.xpath("//a[text()='Login']"));
-        driver.findElement(By.xpath("//a[text()='Shipping & returns']"));
-
-        driver.findElement(By.xpath("//a[contains(text(),'Register')]"));
-        driver.findElement(By.xpath("//a[contains(text(),'Shipping')]"));
-        driver.findElement(By.xpath("//a[contains(text(),'& returns')]"));
+        driver.findElement(By.xpath("//a[contains(text(),'Mua')]"));
+        driver.findElement(By.xpath("//a[contains(text(),'khóa')]"));
+        driver.findElement(By.xpath("//a[contains(text(),'học')]"));
 
         driver.findElement(By.xpath("//a"));
         driver.findElement(By.xpath("//button"));
         driver.findElement(By.xpath("//input"));
-
     }
 
     @AfterClass
