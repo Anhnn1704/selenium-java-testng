a0ad1e624a98337c69e16a0c7c81c46b2080163e
Automation Test <anhnn1704@gmail.com>
2025-06-22 07:31:40 +0700

    Update Topic 01

--- selenium/webdriver/Topic_03_XPath_Css_Part_02.java
+++ selenium/webdriver/Topic_03_XPath_Css_Part_02.java
@@ -24,7 +24,6 @@ public class Topic_03_XPath_Css_Part_02 {
 
     @Test
     public void xPath(){
-        driver = new FirefoxDriver();
         driver.get("https://tanvietbooks.vn/account/register");
          //Tiêu chí chọn: duy nhất - id/class/name - giá trị của attribute phải có nghĩa (liên quan tới element đó)
         //Relative Xpath Format: //tagname[@attribute='value']
@@ -35,7 +34,7 @@ public class Topic_03_XPath_Css_Part_02 {
         driver.navigate().back();
 
         //2. Hàm text()
-        driver.findElement(By.xpath("//p[text()='Không có sản phẩm nào.']"));
+        driver.findElement(By.xpath("//a[text()='Bách khoa cho bé']"));
 
         //3. Hàm contains()
         driver.findElement(By.xpath("//a[contains(text(), 'sách công nghệ')]"));
@@ -53,49 +52,13 @@ public class Topic_03_XPath_Css_Part_02 {
         driver.findElement(By.xpath("//input[@id='lastName' or @name='firstName']"));
 
         //7. Xpath Axes
-        //Xpath: //button[@title='Mua ngay']/parent::*/preceding-sibling::a[@title='CON ĐƯỜNG TỈNH THỨC']
-        driver.findElement(By.xpath("//button[@title='Mua ngay']/parent::*/preceding-sibling::a[@title='CON ĐƯỜNG TỈNH THỨC']")).click();
-    }
-
-    @Test
-    public void xPath1(){
-        driver.get("https://nhanam.vn/account/register");
-        //driver.get("https://nhanam.vn/triet-hoc");
-        //Tiêu chí chọn: duy nhất - id/class/name - giá trị của attribute phải có nghĩa (liên quan tới element đó)
-        //Relative Xpath Format: //tagname[@attribute='value']
-
-        //1. Đối với các element che mất/bị ẩn/không nằm trong viewport (nằm phía dưới hoặc phía trên màn hình đang chạy) / không tìm được thuộc tính trong thẻ là duy nhất => Tìm thuộc tính trong element cha của nó
-        driver.findElement(By.xpath("//li[@class='li_menu']//a[@title='Về Nhã Nam']")).click();
-        driver.navigate().back();
-
-        //2. Hàm text()
-        driver.findElement(By.xpath("//p[text()='Đổi trả dễ dàng']"));
-
-        //3. Hàm contains()
-        driver.findElement(By.xpath("//p[contains(text(), 'Tích điểm')]"));
-        driver.findElement(By.xpath("//a[contains(@title,'độc giả')]"));
-        driver.findElement(By.xpath("//a[contains(@title,'độc giả')]"));
-        driver.findElement(By.xpath("//ul[@class='list-menu']//a[contains(@href,'tuyen')]"));
-        driver.findElement(By.xpath("//a[contains(.,'Nhã')]"));
-        driver.findElement(By.xpath("//a[contains(string(),'Nhã')]"));
-
-        //4. Hàm concat()
-        // Ví dụ: Hello "Mai", What's happened?
-        //xpath: //p[text()=concat('Hello "Mai" What',"'s happened?")]
-
-        //5. Hàm starts-with: dùng khi thuộc tính của 1 element với mỗi lần lấy xpath nó thay đổi liên tục nhưng text ở đâu luôn cố định
-        // Ví dụ: màn Login của web Lazada với trường Nhập Your phone or Email
-        //xpath: //input[starts-with(@data-spm-anchor-id,'a2o4n.tm80151110')]
-
-        //6. Hàm AND OR NOT
-        driver.findElement(By.xpath("//input[@id='lastName' and @placeholder='Họ']"));
-        driver.findElement(By.xpath("//input[@id='lastName' or @name='firstName']"));
-
-        //7. Xpath Axes
-        //Xpath: //button[@title='Mua ngay']/parent::*/preceding-sibling::a[@title='CON ĐƯỜNG TỈNH THỨC']
-        driver.findElement(By.xpath("//a[@title='Kí Họa Venice']")).click();
+        driver.findElement(By.id("query")).sendKeys("Kí họa");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         driver.findElement(By.xpath("//button[@title='Thêm vào giỏ hàng']/parent::*/preceding-sibling::a[@title='Kí Họa Venice']")).click();
     }
 
+    @AfterClass
+    public void cleanBrowser(){
+        driver.quit();
+    }
 }
